9ddb483c94238e86012b5db4ebf3ce3d12722e57
mzieniewicz <mzieniewicz@gmail.com>
2020-06-07 21:32:45 +0200

    add FurumUser test

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.spring.forum;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class ForumUserTestSuite {
+    @Test
+    public void testGetUsername(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+        //When
+        String name = forumUser.getUsername();
+        //Then
+        Assert.assertEquals("John Smith",name);
+    }
+}
